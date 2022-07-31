4b32675c2553b4dc36d6b166365323f6282e0350
Moneet Tiwana <mtiwana4@uwo.ca>
2022-07-31 17:40:48 -0400

    Adding complete portfolio template

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
+
+// TODO: 17. Creating the portfolio item page: https://youtu.be/_xkSvufmjEs?t=7235
